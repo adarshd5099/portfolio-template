b7a547defb55647bbd6368476b524068e2638c34
Adarsh D <adarshd181998@gmail.com>
2019-12-24 01:24:09 +0530

    file added

---
+++ scripts/index.js
@@ -0,0 +1,25 @@
+function openmodal(n){
+
+	switch(n){
+		case 1:$('#work1').modal();
+		break;
+		case 2:$('#work2').modal();
+		break;
+		case 3:$('#work3').modal();
+		break;
+		case 4:$('#work4').modal();
+		break;
+		case 5:$('#work5').modal();
+		break;
+		case 6:$('#work6').modal();
+		break;
+		case 7:$('#work7').modal();
+		break;
+		case 8:$('#work8').modal();
+		break;
+
+	}
+	
+	
+}
+
